41eae38c504f5eeccff9ce14d6ad5deffda9bf08
Mohammed Shakeel <nmohammedshakeel22@gmail.com>
2025-12-04 20:54:07 +0530

    Completed Employee Management System

--- employee-management/src/main/java/com/example/util/DBConnection.java
+++ employee-management/src/main/java/com/example/util/DBConnection.java
@@ -1,33 +1,23 @@
 package com.example.util;
 
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.Properties;
 
 public class DBConnection {
 
-    private static final Properties props = loadProperties();
+    private static final String URL = "jdbc:mysql://localhost:3306/employee_db";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "MSmysql@1";
 
-    private static Properties loadProperties() {
-        Properties properties = new Properties();
-        try (InputStream input = DBConnection.class.getClassLoader().getResourceAsStream("db.properties")) {
-
-            if (input == null) {
-                throw new RuntimeException("Sorry, unable to find db.properties. " + "Make sure it's in src/main/resources/");
-            }
-            properties.load(input);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to load database configuration", e);
-        }
-        return properties;
-    }
+    // static {
+    //     try {
+    //         Class.forName("com.mysql.cj.jdbc.Driver"); 
+    //     } catch (ClassNotFoundException e) {
+    //         throw new RuntimeException("MySQL Driver not found!", e);
+    //     }
+    // }
 
     public static Connection getConnection() throws java.sql.SQLException {
-        String url = props.getProperty("db.url");
-        String username = props.getProperty("db.username");
-        String password = props.getProperty("db.password");
-
-        return DriverManager.getConnection(url, username, password);
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
 }
